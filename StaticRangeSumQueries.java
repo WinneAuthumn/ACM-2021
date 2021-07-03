8e2ba7bb54b143b1b627e6baa569276d2415e466
Lê Hồ Thu Quỳnh <77103932+WinneAuthumn@users.noreply.github.com>
2021-07-04 02:21:11 +0700

    Add files via upload

---
+++ StaticRangeSumQueries.java
@@ -0,0 +1,131 @@
+import java.util.*;
+import java.io.*;
+
+public class StaticRangeSumQueries {
+
+	public static void main(String[] args) {
+		StringBuilder outputBuffer = new StringBuilder();
+		int noOfValues = ni();
+		int queries = ni();
+
+		long[] arrayValues = new long[noOfValues + 1];
+		arrayValues[0] = 0;
+		for (int i = 1; i <= noOfValues; i++) {
+			arrayValues[i] = nl() + arrayValues[i - 1];
+		}
+
+		while (queries > 0) {
+			int left = ni();
+			int right = ni();
+			long sum = arrayValues[right] - arrayValues[left - 1];
+
+			outputBuffer.append(sum).append("\n");
+			queries--;
+		}
+
+		System.out.println(outputBuffer);
+	}
+
+	static InputStream is = System.in;
+
+	static byte[] inbuf = new byte[1 << 24];
+	static int lenbuf = 0, ptrbuf = 0;
+
+	static int readByte() {
+		if (lenbuf == -1)
+			throw new InputMismatchException();
+		if (ptrbuf >= lenbuf) {
+			ptrbuf = 0;
+			try {
+				lenbuf = is.read(inbuf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (lenbuf <= 0)
+				return -1;
+		}
+		return inbuf[ptrbuf++];
+	}
+
+	static boolean isSpaceChar(int c) {
+		return !(c >= 33 && c <= 126);
+	}
+
+	static int skip() {
+		int b;
+		while ((b = readByte()) != -1 && isSpaceChar(b))
+			;
+		return b;
+	}
+
+	static float nf() {
+		return Float.parseFloat(ns());
+	}
+
+	static double nd() {
+		return Double.parseDouble(ns());
+	}
+
+	static char nc() {
+		return (char) skip();
+	}
+
+	static String ns() {
+		int b = skip();
+		StringBuilder sb = new StringBuilder();
+		while (!(isSpaceChar(b))) {
+			sb.appendCodePoint(b);
+			b = readByte();
+		}
+		return sb.toString();
+	}
+
+	static char[] ns(int n) {
+		char[] buf = new char[n];
+		int b = skip(), p = 0;
+		while (p < n && !(isSpaceChar(b))) {
+			buf[p++] = (char) b;
+			b = readByte();
+		}
+		return n == p ? buf : Arrays.copyOf(buf, p);
+	}
+
+	static int ni() {
+		int num = 0, b;
+		boolean minus = false;
+		while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+			;
+		if (b == '-') {
+			minus = true;
+			b = readByte();
+		}
+		while (true) {
+			if (b >= '0' && b <= '9') {
+				num = num * 10 + (b - '0');
+			} else {
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+
+	static long nl() {
+		long num = 0;
+		int b;
+		boolean minus = false;
+		while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+			;
+		if (b == '-') {
+			minus = true;
+			b = readByte();
+		}
+		while (true) {
+			if (b >= '0' && b <= '9') {
+				num = num * 10 + (b - '0');
+			} else {
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+}
